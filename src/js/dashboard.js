d2633d074c30b3009ce2f5eb1ede57c8857b603f
Jeremy Felt <jeremy.felt@gmail.com>
2017-03-01 18:17:48 -0800

    Continue bootstrapping initial views

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,32 +1,39 @@
 /* global console */
 ( function( $ ) {
-	var body = {
-		"size": 0,
-		"aggs": {
-			"top_codes": {
-				"terms": {
-					"field": "code"
+	var aggregateRequest = function( type ) {
+		var body = {
+			"size": 0,
+			"aggs": {
+				"top_codes": {
+					"terms": {
+						"field": type
+					}
 				}
 			}
-		}
+		};
+
+		$.ajax( {
+			url: "https://elastic.wsu.edu/a11y-scan/record/_search",
+			type: "POST",
+			crossDomain: true,
+			dataType: "json",
+			data: JSON.stringify( body ),
+			success: function( response ) {
+				var buckets = response.aggregations.top_codes.buckets;
+				var container = $( "#" + type + "-display-container" );
+
+				for ( var i = 0, x = buckets.length; i < x; i++ ) {
+					container.append( "<div class='result'><span class='count'>" + buckets[ i ].doc_count + "</span><span class='" + type + "'>" + decodeURIComponent( buckets[ i ].key ) + "</span></div>" );
+				}
+			},
+			error: function( jqXHR, textStatus, errorThrown ) {
+				console.log( jqXHR, textStatus, errorThrown );
+			}
+		} );
 	};
 
-	$.ajax( {
-		url: "https://elastic.wsu.edu/a11y-scan/record/_search",
-		type: "POST",
-		crossDomain: true,
-		dataType: "json",
-		data: JSON.stringify( body ),
-		success: function( response ) {
-			var buckets = response.aggregations.top_codes.buckets;
-			var container = $( "#code-display-container" );
+	aggregateRequest( "code" );
+	aggregateRequest( "domain" );
+	aggregateRequest( "url" );
 
-			for ( var i = 0, x = buckets.length; i < x; i++ ) {
-				container.append( "<div class='result'><span class='count'>" + buckets[ i ].doc_count + "</span><span class='code'>" + buckets[ i ].key + "</span></div>" );
-			}
-		},
-		error: function( jqXHR, textStatus, errorThrown ) {
-			console.log( jqXHR, textStatus, errorThrown );
-		}
-	} );
 }( jQuery ) );
