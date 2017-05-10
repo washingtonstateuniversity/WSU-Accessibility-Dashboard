3c5936531ecdb7d1fc06ee2f0a63c3c5d63b3f3d
Jeremy Felt <jeremy.felt@gmail.com>
2017-05-10 15:05:59 -0700

    Navigate through hash changes rather than page loads

--- src/js/components/typeListItem.jsx
+++ src/js/components/typeListItem.jsx
@@ -3,12 +3,19 @@ class TypeListItem extends React.Component {
 		super( props );
 	}
 
+	handleClick( e ) {
+		e.preventDefault();
+		console.log( e.target.dataset.hash );
+		window.location.hash = "/" + e.target.dataset.hash;
+	}
+
 	render() {
-		let url = "/#/" + this.props.type + "/" + this.props.code;
+		let hash = this.props.type + "/" + this.props.code;
+		let url = "/#/" + hash;
 
 		return 	<li className="result">
 			<span className="count">{this.props.count}</span>
-			<a className={this.props.type} data-code={this.props.selector} href={url} >{this.props.name}</a>
+			<a className={this.props.type} onClick={this.handleClick} data-code={this.props.selector} data-hash={hash} href={url} >{this.props.name}</a>
 		</li>
 	}
 }
