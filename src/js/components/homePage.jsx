ed1887908a41c0a7aff2b936e0ebf2ab9aedc028
Jeremy Felt <jeremy.felt@gmail.com>
2017-05-10 14:54:06 -0700

    Show 15 of each record type on the home page

--- src/js/components/homePage.jsx
+++ src/js/components/homePage.jsx
@@ -7,9 +7,9 @@ class HomePage extends React.Component {
 
 	render() {
 		return 	<div className="dashboard-wrapper">
-			<TypeOverview title="Top Codes" type="code" count="10" />
-			<TypeOverview title="Top Selectors" type="selector" count="10" />
-			<TypeOverview title="Top Domains" type="domain" count="10" />
+			<TypeOverview title="Top Codes" type="code" count="15" />
+			<TypeOverview title="Top Selectors" type="selector" count="15" />
+			<TypeOverview title="Top Domains" type="domain" count="15" />
 		</div>
 	}
 }
