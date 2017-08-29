dd307e5bd018b424a5d70842c124cb3a8aef0d62
Jeremy Felt <jeremy.felt@gmail.com>
2017-08-28 17:56:00 -0700

    Add hyperlinks to domains on main view

--- src/js/components/domain.jsx
+++ src/js/components/domain.jsx
@@ -3,15 +3,28 @@ class Domain extends React.Component {
         super( props );
     }
 
+    handleClick( e ) {
+        e.preventDefault();
+        window.location.hash = "/" + e.target.dataset.hash;
+    }
+
     render() {
         let error_rate = 0;
+        let hash = "domain/" + this.props.domain;
+        let url = "#/" + hash;
 
         if ( this.props.a11y_count !== 0 && this.props.error_count !== 0 ) {
             error_rate = ( this.props.error_count / this.props.a11y_count ).toFixed( 2 );
         }
 
         return 	<tr>
-            <td>{this.props.domain}</td><td>{this.props.url_count}</td><td>{this.props.a11y_count}</td><td>{this.props.error_count}</td><td>{error_rate}</td>
+            <td><a onClick={this.handleClick}
+                   data-hash={hash}
+                   href={url} >{this.props.domain}</a></td>
+            <td>{this.props.url_count}</td>
+            <td>{this.props.a11y_count}</td>
+            <td>{this.props.error_count}</td>
+            <td>{error_rate}</td>
         </tr>
     }
 }
