d6198ab708e5bafb706ff1f5d0afb46e79bbce58
Jeremy Felt <jeremy.felt@gmail.com>
2017-08-31 11:50:16 -0700

    Provide the truncated URL path in detailed record lists

--- src/js/components/record.jsx
+++ src/js/components/record.jsx
@@ -1,12 +1,20 @@
+let parse_url = require( "url" );
+
 class Record extends React.Component {
 	constructor( props ) {
 		super( props );
 	}
 
 	render() {
+		let url = parse_url.parse( this.props.url );
+
+		if ( 35 < url.path.length ) {
+            url.path = url.path.substring( 0, 35 ) + "...";
+		}
+
 		return 	<tr>
 			<td>{this.props.domain}</td>
-			<td><a href={this.props.url}>{this.props.url}</a></td>
+			<td><a href={this.props.url}>{url.path}</a></td>
 			<td>{this.props.selector}</td>
 			<td>{this.props.type}</td>
 		</tr>
